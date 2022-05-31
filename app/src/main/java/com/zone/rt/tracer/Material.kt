d974c3bfdce7bdf454b9e96b22f7a29d026a7359
Zone <2493920954@qq.com>
2022-05-31 09:10:48 +0800

    schlick approximation

--- app/src/main/java/com/zone/rt/tracer/Material.kt
+++ app/src/main/java/com/zone/rt/tracer/Material.kt
@@ -1,7 +1,9 @@
 package com.zone.rt.tracer
 
 import kotlin.math.min
+import kotlin.math.pow
 import kotlin.math.sqrt
+import kotlin.random.Random
 
 data class ScatterResult(val scatter: Boolean, val scattered: Ray, val attenuation: Color3)
 
@@ -60,11 +62,19 @@ class Dielectric(val ir: Double) : Material() {
         val cos_theta = min(-unitVec dot rec.normal, 1.0)
         val sin_theta = sqrt(1 - cos_theta * cos_theta)
         val cannotRefract = refractionRatio * sin_theta > 1.0
-        val direction = if (cannotRefract) {
+        val direction = if (cannotRefract || reflectance(cos_theta, refractionRatio) > Random.nextDouble(0.0, 1.0)) {
             Vec3.reflect(unitVec, rec.normal)
         } else {
             Vec3.refract(unitVec, rec.normal, refractionRatio)
         }
         return ScatterResult(true, Ray(rec.p, direction), Color3(1.0, 1.0, 1.0))
+    }
+
+    companion object {
+        private fun reflectance(cosine: Double, refIndex: Double): Double {
+            var r0 = (1 - refIndex) / (1 + refIndex)
+            r0 = r0 * r0
+            return r0 + (1 - r0) * (1 - cosine).pow(5.0)
+        }
     }
 }
